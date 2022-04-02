c5726d00498197de8d04ac8fa2da4c2e769d68ba
me <me>
2022-04-02 15:25:42 +0300

    changed user routes

--- admin-api/routes/user.routes.ts
+++ admin-api/routes/user.routes.ts
@@ -1,10 +1,9 @@
 const Router = require('express');
 const router = new Router();
 const userController = require('../controller/user.controller');
-const contentController = require('../controller/content.controller');
 
-router.get('/users/', userController.getUsers)
-router.get('/users/id/:id', userController.getUserByID)
-router.get('/users/email/:email', userController.getUsersByEmail)
+router.get('/', userController.getUsers)
+router.get('/id/:id', userController.getUserByID)
+router.get('/email/:email', userController.getUsersByEmail)
 
 module.exports = router;
