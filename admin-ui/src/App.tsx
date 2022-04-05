e6f07dc2449c84be9b94999096f1e6612f94588f
me <me>
2022-04-05 16:35:56 +0300

    auth page

--- admin-ui/src/App.tsx
+++ admin-ui/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Sidebar from "./components/Sidebar";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import  { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contents from "./pages/Contents";
+import { Login } from './pages/login/Login'
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
@@ -11,9 +13,12 @@ function App() {
         <div className="App">
           <Sidebar/>
             <Routes>
-            <Route path="/Users" element={<Home/>}/>
-            <Route path="/Contents" element={<Contents/>}/>
-
+                <Route path="/Login" element={<Login/>}/>
+                <Route element={<ProtectedRoutes/>}>
+                    <Route path="/Users" element={<Home/>}/>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/Contents" element={<Contents/>}/>
+                </Route>
             </Routes>
         </div>
 
---
+++ admin-ui/src/pages/login/Login.tsx
@@ -0,0 +1,83 @@
+import TextField from '@mui/material/TextField';
+import React, {useState} from 'react';
+import styles from './Login.module.css'
+import Button from '@mui/material/Button';
+import { TailSpin } from 'react-loading-icons'
+import { Outlet } from "react-router-dom";
+import axios from "axios";
+
+const isLogged = (): boolean => {
+    return 0 === document.cookie.indexOf('name=');
+}
+
+const Login = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [fetching, setFetching] = useState<boolean>(false);
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setFetching(true);
+        await axios.post('http://localhost:8080/api/auth/', {
+            email: email,
+            password: password
+        }).then(data => data)
+            .then(res => {
+                setFetching(false);
+                document.cookie = `name=${res.data}; max-age=86400; path=/;`
+                return res.data;
+            }).catch(e => {
+                setFetching(false);
+            })
+    }
+
+    return isLogged() ? <Outlet/> :
+        (
+            <div className={styles.login}>
+                <form onSubmit={handleSubmit} style={{
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}>
+                    <TextField
+                        className={styles.formInput}
+                        size='medium'
+                        label="email"
+                        margin='dense'
+                        multiline={false}
+                        inputProps={{
+                            readOnly: fetching,
+                            autoComplete: 'off'
+                        }}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <TextField
+                        className={styles.formInput}
+                        type="password"
+                        size='medium'
+                        label="password"
+                        margin='dense'
+                        multiline={false}
+                        inputProps={
+                            { readOnly: fetching }
+                        }
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    {
+                        fetching ?
+                        <TailSpin className="spinner" stroke="#678DA6" strokeWidth="3px"/> :
+                        <Button
+                            type="submit"
+                            color="primary"
+                            variant="outlined"
+                            disabled={fetching}
+                        >Login</Button>
+                    }
+                </form>
+        </div>
+    );
+};
+
+export {Login, isLogged};
