4d7140e1ac80f61c9b59bd907ac93f13a9f7ac8f
prcstak <g.marsel2002@gmail.com>
2022-05-28 16:10:02 +0300

    add validation to Create Playlist

--- admin-ui/src/pages/CreatePlaylist/CreatePlaylist.tsx
+++ admin-ui/src/pages/CreatePlaylist/CreatePlaylist.tsx
@@ -8,12 +8,11 @@ import Select from '@mui/material/Select';
 
 import {useDropzone} from "react-dropzone";
 import axios from "axios";
-import Dropzone from "../../components/Dropzone/Dropzone";
 import {getToken, queryConfig, queryConfigMultipart} from "../../components/QueryConfig";
 import RedirectButton from "../../components/CustomButtons/RedirectButton";
-import SimpleButton from "../../components/CustomButtons/SimpleButton";
 import {TailSpin} from "react-loading-icons";
 import CoverUpload from "../../components/Dropzone/CoverUpload";
+import {Form} from "react-bootstrap";
 
 const StyledTextField = styled(TextField)`
       width: 500px;
@@ -22,9 +21,9 @@ const StyledTextField = styled(TextField)`
 
 
 const CreatePlaylist = () => {
-    const [id, setId] = useState('');
-    const [title, setTitle] = useState('');
-    const [type, setType] = useState('');
+    const [id, setId] = useState<string|undefined>();
+    const [title, setTitle] = useState<string|undefined>();
+    const [type, setType] = useState<string|undefined>('album');
     const [paths, setPaths] = useState([]);
     const [uploading, setUploading] = useState<boolean>(false);
 
@@ -42,7 +41,8 @@ const CreatePlaylist = () => {
 
 
 
-    const createPlaylist =  () => {
+    const createPlaylist =  (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
         setUploading(true);
         const form: any = new FormData();
         let config = queryConfig(token);
@@ -71,46 +71,59 @@ const CreatePlaylist = () => {
             <RedirectButton page={'content'} text={"BACK"}/>
             <h1 className={styles.title}>New Playlist</h1>
             <div className={styles.form}>
-                <FormControl>
+                <Form onSubmit={createPlaylist}>
+                    <FormControl>
 
-                <CoverUpload {...dropData} filetype={"cover"} size={1} cover={paths}/>
+                        <CoverUpload {...dropData} filetype={"cover"} size={1} cover={paths}/>
 
-                <StyledTextField
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    color={"secondary"}
-                    label={'Title'}
-                />
-                <FormControl color={"secondary"} sx={{ml: 34, mt: 2}}>
-                    <InputLabel id="demo-simple-select-label">Type</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={type}
-                        label="Type"
-                        onChange={(e)=>setType(e.target.value)}
-                    >
-                        <MenuItem value={'album'}>Album</MenuItem>
-                        <MenuItem value={'single'}>Single</MenuItem>
-                        <MenuItem value={'ep'}>EP</MenuItem>
-                    </Select>
-                </FormControl>
-                <StyledTextField
-                    onChange={(e) => setId(e.target.value)}
-                    color={"secondary"}
-                    label={'Artist Id'}
-                    value={id}
-                />
-                    {
-                        uploading?
-                            <TailSpin
-                                className={styles.spinner}
-                                stroke="#678DA6"
-                                strokeWidth="2px"/>
-                            :
-                            <SimpleButton onClick={createPlaylist} text={"CREATE"}/>
-                    }
-                </FormControl>
+                        <StyledTextField
+                            onBlur={(e) => setTitle(e.target.value)}
+                            type={'text'}
+                            color={"secondary"}
+                            label={'Title'}
+                            value={title}
+                            inputProps={{
+                                readOnly: false,
+                                autoComplete: 'off'
+                            }}
+                            required
+                        />
+                        <FormControl color={"secondary"} sx={{ml: 34, mt: 2}}>
+                            <InputLabel id="demo-simple-select-label">Type</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={type}
+                                label="Type"
+                                onChange={(e)=>setType(e.target.value)}
+                                required
+                            >
+                                <MenuItem value={'album'}>Album</MenuItem>
+                                <MenuItem value={'single'}>Single</MenuItem>
+                                <MenuItem value={'ep'}>EP</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <StyledTextField
+                            onBlur={(e) => setId(e.target.value)}
+                            color={"secondary"}
+                            label={'Artist Id'}
+                            value={id}
+                            type="number"
+                            required
+                        />
+                        {
+                            uploading?
+                                <TailSpin
+                                    className={styles.spinner}
+                                    stroke="#678DA6"
+                                    strokeWidth="2px"/>
+                                :
+                                <button className={`${styles.info} ${styles.update}`}>
+                                    CREATE
+                                </button>
+                        }
+                    </FormControl>
+                </Form>
             </div>
         </div>
     );
